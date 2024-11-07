60b786336639809caf374d9e3f75d42e98bb850d
Javier Enriquez <pedido.aurea@gmail.com>
2024-11-07 12:14:46 -0500

    ordenar estructura web

---
+++ mainhead.php
@@ -0,0 +1,15 @@
+ <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- vendor css -->
+    <link href="../../public/lib/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="../../public/lib/Ionicons/css/ionicons.css" rel="stylesheet">
+    <link href="../../public/lib/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
+    <link href="../../public/lib/jquery-switchbutton/jquery.switchButton.css" rel="stylesheet">
+    
+    <link href="../../public/lib/datatables/jquery.dataTables.css" rel="stylesheet">
+    <link href="../../public/datatables/buttons.dataTables.min.css" rel="stylesheet">
+    
+    <!-- Bracket CSS -->
+    <link rel="stylesheet" href="../../public/css/bracket.css">
